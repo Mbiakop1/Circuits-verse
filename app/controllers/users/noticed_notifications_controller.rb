534da662a4b4e2a9bbed0e10f57ecb062c62d5f9
Mbiakop Clinton <mbiakopclinton@gmail.com>
2022-10-18 10:35:54 +0100

    add files

---
+++ app/controllers/users/noticed_notifications_controller.rb
@@ -0,0 +1,22 @@
+# frozen_string_literal: true
+
+class Users::NoticedNotificationsController < ApplicationController
+  before_action :authenticate_user!
+
+  def index
+    @notifications = NoticedNotification.where(recipient: current_user).newest_first
+    @unread = NoticedNotification.where(recipient: current_user).newest_first.unread
+  end
+
+  def mark_as_read
+    notification = NoticedNotification.find(params[:notification_id])
+    notification.update(read_at: Time.zone.now)
+    project = notification.params[:project]
+    redirect_to user_project_path(project.author, project)
+  end
+
+  def mark_all_as_read
+    NoticedNotification.where(recipient: current_user, read_at: nil).update_all(read_at: Time.zone.now) # rubocop:disable Rails/SkipsModelValidations
+    redirect_to notifications_path(current_user)
+  end
+end
---
+++ app/decorators/profile_decorator.rb
@@ -0,0 +1,28 @@
+# frozen_string_literal: true
+
+class ProfileDecorator < SimpleDelegator
+  def profile
+    __getobj__
+  end
+
+  def member_since
+    ((Time.now.to_i - profile.created_at.to_i) / 1.day)
+  end
+
+  def educational_institute
+    profile.educational_institute || "Not Entered"
+  end
+
+  def country_name
+    country = ISO3166::Country[profile.country]
+    country ? country.translations[I18n.locale.to_s] || country.name : "Not Entered"
+  end
+
+  def mail_subscription
+    profile.subscribed? ? "Subscribed" : "Not Subscribed"
+  end
+
+  def total_circuits
+    profile.projects.exists? ? profile.projects.count : "No circuits"
+  end
+end
