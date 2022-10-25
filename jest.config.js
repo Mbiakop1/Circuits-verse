ed6cfb927e7d4e494e3d8699eb7911b160e92d3c
Mbiakop Clinton <mbiakopclinton@gmail.com>
2022-10-25 13:37:52 +0100

    update repository

--- jest.config.js
+++ jest.config.js
@@ -1,9 +1,10 @@
-module.exports = {
-    setupFiles: ['./jest.setup.js'],
-    testMatch: ['**/spec/**/*.spec.js'],
-    verbose: true,
-    moduleNameMapper: {
-        '\\.(css|less|sass|scss)$': 'identity-obj-proxy',
-        'typeface-nunito': 'identity-obj-proxy',
-    },
-};
+const format = (date, locale, options) =>
+        new Intl.DateTimeFormat(locale, options).format(date)
+
+const now = new Date()
+
+format(now, 'es') // '5/1/2022'
+format(now, 'es', { dateStyle: 'long'}) // "5 January de 2022'
+format(now, 'es', { timeStyle: 'short'}) // '14:58"
+format(now, 'ko') // ‘2022. 1. 5.'
+format(now, 'en', {wednesday: 'short', day: 'numeric'}) // '5 wed
